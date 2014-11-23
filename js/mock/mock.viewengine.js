5b7e2395026ef664be747a4ae661cd0511989a3b
Chris Love <clove@extremewebworks.com>
2014-11-22 19:44:21 -0500

    adding tests and a caching service

---
+++ js/mock/mock.viewengine.js
@@ -0,0 +1,141 @@
+﻿//Mock View Engine to test spa library
+
+;
+
+(function (window, undefined) {
+
+    "use strict";
+
+    var MockViewEngine = function () {
+
+        return new MockViewEngine.fn.init();
+
+    };
+
+    MockViewEngine.fn = MockViewEngine.prototype = {
+
+        constructor: MockViewEngine,
+
+        init: function () {
+
+            return this;
+        },
+
+        version: "0.0.1",
+
+        eventPrefix: "spa-",
+        templateType: "[type='text/x-Mock-template']",
+        appPrefix: "MockApp-",
+
+        views: {},
+
+        parseViews: function (remove) {
+
+            console.log("parseView called, remove = " + remove);
+
+        },
+
+        getView: function (viewId) {
+            return this.views[viewId];
+        },
+
+        getViews: function () {
+            return this.views
+        },
+
+        setView: function (viewId, view) {
+
+            if (typeof view === "string") {
+
+                this.views[viewId] = view;
+                this.saveViews();
+
+            }
+
+        },
+
+        setViews: function (views) {
+
+            var mockViewEngine = this,
+                view;
+
+            for (view in views) {
+                mockViewEngine.setView(view, views[view]);
+            }
+
+        },
+
+        addViews: function (views) {
+
+            var mockViewEngine = this,
+                name, copy;
+
+            for (name in views) {
+
+                copy = views[name];
+
+                // Prevent never-ending loop
+                if (mockViewEngine.views === copy) {
+                    continue;
+                }
+
+                if (copy !== undefined) {
+                    mockViewEngine.views[name] = copy;
+                }
+            }
+
+            mockViewEngine.saveViews();
+
+        },
+
+        saveViews: function () {
+
+            var mockViewEngine = this;
+
+            localStorage.setItem(mockViewEngine.appPrefix + "views", JSON.stringify(mockViewEngine.views));
+
+        },
+
+        removeView: function (key) {
+
+            delete this.views[key];
+            this.saveViews();
+
+        },
+
+        bind: function (targetSelector, templateName, model) {
+
+            if ((typeof targetSelector !== "string") ||
+               (typeof templateName !== "string") ||
+                model === undefined) {
+
+                throw {
+                    Name: "MockViewEngine Error",
+                    Description: "missing argument in mergeData"
+                }
+
+                return;
+            }
+
+            var mockViewEngine = this;
+
+            if (mockViewEngine.views[templateName]) {
+
+                console.log("we have a match to the view");
+
+            } else {
+
+                console.error("we do not have a view match");
+
+            }
+
+        }
+
+    };
+
+    // Give the init function the mockViewEngine prototype for later instantiation
+    mockViewEngine.fn.init.prototype = MockViewEngine.fn;
+
+    return (window.MockViewEngine = MockViewEngine);
+
+})(window);
---
+++ js/specs/test.bootstrap.js
@@ -0,0 +1,108 @@
+﻿var rivetsVE = RivetsViewEngine({ "appName": "spa-test" }),
+        _spa,
+        testApp = new function () { };
+
+testApp.childView1 = Controller.extend({
+
+    init: function (rootScope) {
+
+        this._super(rootScope);
+
+    },
+
+    onload: function (params) {
+
+    }
+
+
+});
+
+testApp.childView2 = Controller.extend({
+
+    init: function (rootScope) {
+
+        this._super(rootScope);
+
+    },
+
+    onload: function (params) {
+
+    }
+
+
+});
+
+testApp.childView3 = Controller.extend({
+
+    init: function (rootScope) {
+
+        this._super(rootScope);
+
+    },
+
+    onload: function (params) {
+
+    }
+
+
+});
+
+testApp.childView4 = Controller.extend({
+
+    init: function (rootScope) {
+
+        this._super(rootScope);
+
+    },
+
+    onload: function (params) {
+
+    }
+
+
+});
+
+testApp.simpleview1 = Controller.extend({
+
+    init: function (rootScope) {
+
+        this._super(rootScope);
+
+    },
+
+    onload: function (params) {
+
+    }
+
+
+});
+
+
+testApp.simpleview2 = Controller.extend({
+
+    init: function (rootScope) {
+
+        this._super(rootScope);
+
+    },
+
+    onload: function (params) {
+
+    }
+
+
+});
+
+
+
+
+_spa = SPA({
+        "AppContext": testApp,
+        "viewEngine": rivetsVE,
+        "defaultPage": "simpleView1",
+        "viewWrapper": "#main",
+        "analytics": undefined,
+        "viewTransition": "slide",
+        "defaultTitle": "SPA Test"
+    });
+
